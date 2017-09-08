377fe000fa5fb501a16eeef39d1aed39abc8c92c
dorairajsanjay1 <31754552+dorairajsanjay1@users.noreply.github.com>
2017-09-07 21:01:17 -0700

    my first class work

---
+++ testfolder_cis024c/homework1.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[1]:
+
+x=5
+y=6
+z=x+y
+print(z)
+
+
+# In[2]:
+
+employee1Age = input("Enter employee 1 age:")
+
+
+# In[3]:
+
+print(employee1Age)
+
+
+# In[4]:
+
+employee1Age
+
+
+# In[ ]:
+
+
+
